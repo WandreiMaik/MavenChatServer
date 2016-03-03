283c6945d288ed076313b0452961205ff06a8fa1
Wandrei <maik.wandrei@siemens.com>
2016-03-03 14:47:03 +0100

    First commit

---
+++ src/main/java/info/sperber/App.java
@@ -0,0 +1,50 @@
+package info.sperber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static List<Socket> clients;
+
+    public static void main( String[] args ) throws IOException {
+        ServerSocket server = new ServerSocket(4321);
+
+        System.out.printf("Starte Anwendung.");
+
+        clients = new ArrayList<Socket>();
+
+        while (true)
+        {
+            try
+            {
+                clients.add(server.accept());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            new Thread(new EchoThread(clients.get(clients.size() - 1))).start();
+        }
+    }
+
+    public static void broadcast(String message) throws IOException {
+        for(Socket client : clients)
+        {
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+
+            out.println(String.format("%s", message));
+        }
+    }
+}
---
+++ src/main/java/info/sperber/EchoThread.java
@@ -0,0 +1,41 @@
+package info.sperber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.Timestamp;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Created by z003aknc on 03.03.2016.
+ */
+public class EchoThread implements Runnable
+{
+    protected Socket _client;
+
+    public EchoThread(Socket client)
+    {
+        _client = client;
+    }
+
+    public void run()
+    {
+        try
+        {
+            Scanner in = new Scanner(_client.getInputStream());
+
+            PrintWriter out = new PrintWriter(_client.getOutputStream(), true);
+
+            String input = in.nextLine();
+
+            App.broadcast(new Date(System.currentTimeMillis()).toString() + ": " + input);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return;
+    }
+}
